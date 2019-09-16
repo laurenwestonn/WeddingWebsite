04a897a493df399e0941ab70341652e8d9f61d17
laurenweston <lauren-weston@hotmail.co.uk>
2019-09-16 19:14:26 +0100

    Updates

---
+++ precache-manifest.83895500e9a569f597e753e1680522b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5ad0e5082d0197655b29d0679a5675",
+    "url": "/WeddingWebsite/index.html"
+  },
+  {
+    "revision": "c1d253a0773f80b41878",
+    "url": "/WeddingWebsite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1853a4faa7b5a3dad0c1",
+    "url": "/WeddingWebsite/static/js/2.a2ccf0ba.chunk.js"
+  },
+  {
+    "revision": "c1d253a0773f80b41878",
+    "url": "/WeddingWebsite/static/js/main.af976e17.chunk.js"
+  },
+  {
+    "revision": "abfd7096593776175c55",
+    "url": "/WeddingWebsite/static/js/runtime~main.806bbfdf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WeddingWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
