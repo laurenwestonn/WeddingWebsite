c4bd00fd35cffb394e3f89dcce6f67bec93e1b64
laurenweston <lauren-weston@hotmail.co.uk>
2019-09-20 15:45:07 +0100

    Better file structue

--- wedding-website/src/App.js
+++ wedding-website/src/App.js
@@ -1,32 +1,9 @@
 import React from "react";
 import "./scss/home.scss";
-import RobAndLauren from "./images/RobAndLauren.jpg";
+import HomePage from "./js/HomePage";
 
 function App() {
-  return (
-    <>
-      <div className="row no-gutters">
-        <div className="col-12">
-          <div className="cover-photo-container">
-            <div
-              className="photo"
-              style={{
-                backgroundImage:
-                  "linear-gradient(rgba(0,0, 0, 0.5), rgba(0, 0, 0, 0.8)),url(" +
-                  RobAndLauren +
-                  ")"
-              }}
-            ></div>
-            <div className="overlay"></div>
-            <div className="content">
-              <div className="title">Rob & Lauren</div>
-              <div className="subtitle">5th July 2020</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <HomePage />;
 }
 
 export default App;
---
+++ wedding-website/src/js/HomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CoverPhoto from "./CoverPhoto";
+import RobAndLauren from "../images/RobAndLauren.jpg";
+
+function HomePage(props) {
+  return <CoverPhoto coverPhoto={RobAndLauren} />;
+}
+
+export default HomePage;
