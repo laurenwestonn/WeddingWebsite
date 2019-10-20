495ea669ac5100ea3b377ee624ba7ac03e915472
laurenweston <lauren.weston@cantarus.com>
2019-10-20 16:12:09 +0100

    Countdown logic

--- wedding-website/src/js/components/Countdown.js
+++ wedding-website/src/js/components/Countdown.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
 
-function Countdown() {
+function Countdown(props) {
+  const [days, setDays] = useState(null);
+  const [hours, setHours] = useState(null);
+  const [minutes, setMinutes] = useState(null);
+  const [seconds, setSeconds] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let msToHours = 1000 * 3600;
+
+      let msBetween = props.countdownFrom - new Date();
+
+      setDays(Math.floor(msBetween / (msToHours * 24)));
+      let daysRemainder = msBetween % (msToHours * 24);
+
+      setHours(Math.floor(daysRemainder / msToHours));
+      let hoursRemainder = daysRemainder % msToHours;
+
+      setMinutes(Math.floor(hoursRemainder / (1000 * 60)));
+      let minutesRemainder = hoursRemainder % (1000 * 60);
+
+      setSeconds(Math.floor(minutesRemainder / 1000));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [props.countdownFrom]);
+
   return (
     <div className="countdown">
       <div className="countdown-info">
@@ -13,24 +39,38 @@ function Countdown() {
       </div>
 
       <div className="countdown-breakdown">
-        <div className="box">
-          <div className="value">213</div>
-          <div className="measurement">days</div>
-        </div>
-        <div className="box">
-          <div className="value">17</div>
-          <div className="measurement">hours</div>
-        </div>
-        <div className="box">
-          <div className="value">59</div>
-          <div className="measurement">minutes</div>
-        </div>
-        <div className="box">
-          <div className="value">21</div>
-          <div className="measurement">seconds</div>
+        <div className="row no-gutters">
+          <div className="col-6 col-sm-3">
+            <div className="box ml-auto mr-2 my-2 mx-sm-2">
+              <div className="value">{days}</div>
+              <div className="measurement">days</div>
+            </div>
+          </div>
+          <div className="col-6 col-sm-3">
+            <div className="box ml-2 mr-auto my-2 mx-sm-2">
+              <div className="value">{hours}</div>
+              <div className="measurement">hours</div>
+            </div>
+          </div>
+          <div className="col-6 col-sm-3">
+            <div className="box ml-auto mr-2 my-2 mx-sm-2">
+              <div className="value">{minutes}</div>
+              <div className="measurement">minutes</div>
+            </div>
+          </div>
+          <div className="col-6 col-sm-3">
+            <div className="box ml-2 mr-auto my-2 mx-sm-2">
+              <div className="value">{seconds}</div>
+              <div className="measurement">seconds</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 export default Countdown;
+
+Countdown.propTypes = {
+  countdownFrom: PropTypes.instanceOf(Date),
+};
--- wedding-website/src/js/pages/HomePage.js
+++ wedding-website/src/js/pages/HomePage.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { CoverPhoto, AboutUs, WeddingNavBar, Countdown } from "../components";
-import RobAndLauren2 from "../../images/RobAndLauren2.jpg";
-import RobAndLauren3 from "../../images/RobAndLauren3.jpg";
+import React from 'react';
+import {CoverPhoto, AboutUs, WeddingNavBar, Countdown} from '../components';
+import RobAndLauren2 from '../../images/RobAndLauren2.jpg';
+import RobAndLauren3 from '../../images/RobAndLauren3.jpg';
 
 function HomePage() {
   var aboutUsPeople = [
     {
-      name: "Robert Carter",
+      name: 'Robert Carter',
       image: RobAndLauren2,
       bio:
-        "Once upon a time, Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce elementum elementum erat a blandit."
+        'Once upon a time, Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce elementum elementum erat a blandit.',
     },
     {
-      name: "Lauren Weston",
+      name: 'Lauren Weston',
       image: RobAndLauren3,
       bio:
-        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce elementum elementum erat a blandit."
-    }
+        'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce elementum elementum erat a blandit.',
+    },
   ];
 
   return (
@@ -25,7 +25,7 @@ function HomePage() {
         <WeddingNavBar />
         <CoverPhoto isBig={true} />
         <AboutUs people={aboutUsPeople} />
-        <Countdown />
+        <Countdown countdownFrom={new Date(2020, 6, 5, 14, 0, 0)} />
       </div>
     </>
   );
