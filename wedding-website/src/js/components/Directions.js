f16d34498e1e51017ab7dc225dfb3ca6a3ff8976
laurenweston <lauren-weston@hotmail.co.uk>
2019-10-28 22:06:29 +0000

    directions section

---
+++ wedding-website/src/js/components/Directions.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+function Directions() {
+  return (
+    <div className="directions">
+      <div
+        className="background-wrapper"
+        href="https://www.instagram.com/p/B0_Ai1ohh-l/"
+      >
+        <div
+          className="directions-background "
+          style={{
+            backgroundImage:
+              "linear-gradient(rgba(0,0, 0, 0.8), rgba(0, 0, 0, 0.1)),url(https://scontent.cdninstagram.com/vp/2b5df060e98c8b234fa40fa906082395/5E50BCF0/t51.2885-15/sh0.08/e35/s640x640/67403880_1312597192256359_1555069457954497549_n.jpg?_nc_ht=scontent.cdninstagram.com)"
+          }}
+        ></div>
+      </div>
+
+      <div className="top-layer">
+        <h2>How to get there</h2>
+        <div className="direction-info">
+          Styal lodge is situated in the south of Manchester, in Cheshire East,
+          not far from Manchester Airport.
+        </div>
+        <div className="row">
+          <div className="col-10 offset-1 col-md-5 offset-xl-2 col-xl-3 offset-xxl-3 col-xxl-3">
+            <a
+              className="direction-link"
+              href="https://www.google.com/maps/dir/Deansgate,+Manchester/Styal+Lodge+Weddings,+Station+Road,+Styal+SK9+4JN/@53.4130402,-2.3265686,12z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x487bb1dd30adf0f5:0x1f4fb79c3ed50253!2m2!1d-2.2495394!2d53.4782896!1m5!1m1!1s0x487a4d45eaad5471:0x91c28dafd5cd4d1c!2m2!1d-2.2388137!2d53.3555229!3e0"
+            >
+              <div className="direction-option">
+                <h3 className="option-title">By car</h3>
+                <div className="info">
+                  As the venue is near the airport, the length of journey can
+                  vary by car. It should take just over half an hour from
+                  Manchester, when taking the A5103 or the A56.
+                </div>
+                <div className="map-button">~ View Map ~</div>
+              </div>
+            </a>
+          </div>
+          <div className="col-10 offset-1 offset-md-0 col-md-5 offset-xl-2 col-xl-3 col-xxl-3">
+            <a
+              className="direction-link"
+              href="https://www.google.com/maps/dir/Deansgate,+Manchester/Styal+Lodge+Weddings,+Station+Road,+Styal+SK9+4JN/@53.4038818,-2.2876608,12z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x487bb1dd30adf0f5:0x1f4fb79c3ed50253!2m2!1d-2.2495394!2d53.4782896!1m5!1m1!1s0x487a4d45eaad5471:0x91c28dafd5cd4d1c!2m2!1d-2.2388137!2d53.3555229!3e3"
+            >
+              <div className="direction-option">
+                <h3 className="option-title">By train</h3>
+                <div className="info">
+                  Trains run from Deansgate and Piccadilly, arriving at Styal
+                  station an hour later. From there it's a 15 minute walk, or a
+                  short taxi.
+                </div>
+                <div className="map-button">~ View Map ~</div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Directions;
--- wedding-website/src/js/pages/Location.js
+++ wedding-website/src/js/pages/Location.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { FlowerBorder, Countdown } from "../components";
+import { FlowerBorder, Countdown, Directions } from "../components";
 
 function Location() {
   return (
     <>
       <FlowerBorder />
       <Countdown />
-      <div className="location">
-        <h1 className="title">Location</h1>
-        <p>
-          Our venue is
-          <a href="https://www.styallodge.co.uk/"> Styal Lodge</a>
-        </p>
-      </div>
+      <Directions />
     </>
   );
 }
