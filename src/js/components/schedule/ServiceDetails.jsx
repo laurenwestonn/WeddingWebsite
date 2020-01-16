8f78239ee1cab1f247a56b34a98555b8d344cdcd
Lauren Weston <lauren.weston@cantarus.com>
2020-01-16 09:47:16 +0000

    Yass service details close takes back to selector

--- src/js/components/schedule/ServiceDetails.jsx
+++ src/js/components/schedule/ServiceDetails.jsx
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types'
 
 export const ServiceDetails = (props) => (
   <div className='service-details'>
+    <div
+      className='close-icon'
+      style={{backgroundColor: 'pink', width: '30px', height: '30px'}}
+      onClick={
+        () => {
+          let newServices = [ ...props.services ]
+          newServices[props.index].displayDetails = false
+          props.setServices(newServices)
+        }
+      }
+    >X</div>
     <h2>{props.name}</h2>
     <div className='description'>{props.description}</div>
-    <a className='website' href={props.website}>website.com</a>
+    <a className='website' href={props.website}>{props.website}</a>
     <div className='images'>
       <div className="row no-gutters">
         <div className='col'>
