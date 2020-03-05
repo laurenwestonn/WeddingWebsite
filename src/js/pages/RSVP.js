68a1f3e8da49613675d9fc117446c01e850725e3
Lauren Weston <lauren.weston@cantarus.com>
2020-03-05 09:47:25 +0000

    Update the RSVP message to make it clearer

--- src/js/pages/RSVP.js
+++ src/js/pages/RSVP.js
@@ -5,7 +5,7 @@ export function RSVP() {
   return (
     <div className="rsvp">
       <h1 className="title">Will you attend?</h1>
-      <p className="sub-title">Please reserve your place by March.</p>
+      <p className="sub-title">Please fill this form out for each person in your family that will be attending.<br></br><br></br>Reservations must be completed by March.</p>
       <div className='separator' />
       <RSVPForm />
     </div>
